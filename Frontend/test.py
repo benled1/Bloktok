a4995eb272bd5f7ef0dd1e2b1c5a783d9ddd26b9
Philip Gilhespy <philip@gilhespy.net>
2024-03-23 16:27:31 -0700

    image added

--- Frontend/test.py
+++ Frontend/test.py
@@ -1,19 +1,24 @@
 import tkinter as tk
 import vlc
-
-videoPath = "Frontend/Graphics/testClip.mp4"
+from PIL import Image, ImageTk
 
 class StateApp(tk.Tk):
     def __init__(self):
         super().__init__()
         self.state = False  # Initial state is starting screen
         self.title("Block Talk")
+        self.configure(bg="white")
         self.geometry("500x700")
-        self.create_widgets()
+        self.MakeInitialScreen()
 
-    def create_widgets(self):
-        self.titleLabel = tk.Label(self, text="BLOCK TALK")
-        self.titleLabel.pack(pady=10)
+    def MakeInitialScreen(self):
+        self.titleImagePath = "Frontend/Graphics/BlokTokLogo.png"
+        self.titleImage = Image.open(self.titleImagePath)
+        self.titleImage = self.titleImage.resize((500, 100))
+        self.tkTitleImage = ImageTk.PhotoImage(self.titleImage)
+
+        self.titleLabel = tk.Label(self, image=self.tkTitleImage, bg="white")
+        self.titleLabel.pack()
 
         self.descriptionLabel = tk.Label(self, text="Enter a Reddit URL")
         self.descriptionLabel.pack(pady=10)
@@ -21,10 +26,20 @@ class StateApp(tk.Tk):
         self.urlInput = tk.Text(self)
         self.urlInput.pack(pady=10)
 
-        self.toggle_button = tk.Button(self, text="Generate Brainrot Video", command=self.toggle_state)
+        self.toggle_button = tk.Button(self, text="Generate Brainrot Video", command=self.PlayVideo)
         self.toggle_button.pack()
 
-    def toggle_state(self):
+    def PlayVideo(self):
+        # Get url
+        url = self.urlInput.get(1.0, "end-1c") 
+        print(url)
+
+        # Send url to backend for processing
+
+        # Get returned video
+        videoPath = "Frontend/Graphics/testClip.mp4"
+
+        # Reset the screen
         self.urlInput.destroy()
         self.titleLabel.destroy()
         self.descriptionLabel.destroy()
