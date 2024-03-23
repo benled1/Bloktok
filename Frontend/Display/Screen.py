75e2878889f777433aaa38faf029c025eefe0f3e
Philip Gilhespy <philip@gilhespy.net>
2024-03-23 11:13:33 -0700

    update logo

--- Frontend/Display/Screen.py
+++ Frontend/Display/Screen.py
@@ -7,9 +7,11 @@ def StartDisplay() :
 
     background_colour = (255,255,255)
     (width, height) = (500, 700)
+    icon = pygame.image.load('Frontend/Graphics/hackathonLogo1.png')
 
     screen = pygame.display.set_mode((width, height))
     pygame.display.set_caption('Tiktok Garbage Generator')
+    pygame.display.set_icon(icon)
     screen.fill(background_colour)
 
     return pygame
