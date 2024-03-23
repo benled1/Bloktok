8ca801b0dda6877d645ece807c1c615aa1b59c93
Philip Gilhespy <philip@gilhespy.net>
2024-03-23 14:27:26 -0700

    basic tkinter ui

--- Frontend/Display/Screen.py
+++ Frontend/Display/Screen.py
@@ -1,42 +1,10 @@
 ## Main Display Functionality
 
-# Scale values
-sizeX = 500
-sizeY = 700
-fontSizeLarge = 56
-fontSizeMed = 36
-fontSizeSmall = 24
+def CreateLabel(message, size, frame, tk) :
+    return tk.Label(frame, text = message)
 
-# Colours
-black = (0,0,0)
-white = (255,255,255)
-grey = (177,177,177)
+def CreateButton(message, size, callback, frame, tk) :
+    return tk.Button(frame, text = message, command = callback)
 
-def StartDisplay(pygame) :
-    background_colour = (255,255,255)
-    (width, height) = (sizeX, sizeY)
-    icon = pygame.image.load('Frontend/Graphics/hackathonLogo1.png')
-
-    screen = pygame.display.set_mode((width, height))
-    pygame.display.set_caption('Tiktok Garbage Generator')
-    pygame.display.set_icon(icon)
-    screen.fill(background_colour)
-
-    return screen
-
-def DisplayUi(pygame, screen) :
-    # Fonts
-    largeFont = pygame.font.SysFont("Open Sans", fontSizeLarge)
-    medFont = pygame.font.SysFont("Open Sans", fontSizeMed)
-    smallFont = pygame.font.SysFont("Open Sans", fontSizeSmall)
-
-    DisplayText("TikTok Garbage", sizeX/2, sizeY/4 - fontSizeLarge + 10, largeFont, black, screen)
-    DisplayText("Generator", sizeX/2, sizeY/4, largeFont, black, screen)
-
-    DisplayText("Enter a Reddit post URL", sizeX/2, 3*sizeY/4, smallFont, grey, screen)
-
-def DisplayText(message, x, y, font, colour, screen) :
-    text = font.render( message, True, colour, white)
-    textRect = text.get_rect()
-    textRect.center = (x, y)
-    screen.blit(text, textRect)
+def CreateTextInput(height, width, frame, tk) :
+    return tk.Text(frame)
--- Frontend/FrontendDriver.py
+++ Frontend/FrontendDriver.py
@@ -1,20 +1,26 @@
 ## Driver file
 
-import pygame
+import tkinter as tk 
 from Display import Screen
 
-pygame.init()
+# Scale values
+sizeX = 500
+sizeY = 700
 
-screen = Screen.StartDisplay(pygame)
+urlString = ""
 
-# Main Runtime loop
-running = True
-while running :
-    Screen.DisplayUi(pygame, screen)
+def SaveInput() : 
+    url = textBox.get(1.0, "end-1c") 
+    urlString = url
+    print(urlString)
+  
+frame = tk.Tk() 
+frame.title("TikTok Garbage Generator") 
+frame.geometry("% sx% s" % (sizeX, sizeY)) 
 
-    # Check events code
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+Screen.CreateLabel("TikTok Garbage Generator", 0, frame, tk).pack(pady=100, side=tk.TOP)
+Screen.CreateButton("Test button", 0, SaveInput, frame, tk).pack(pady=100, side = tk.BOTTOM)
+textBox = Screen.CreateTextInput(12, int(sizeX/2), frame, tk)
+textBox.pack(side=tk.BOTTOM)
 
-    pygame.display.update()
+frame.mainloop() 
