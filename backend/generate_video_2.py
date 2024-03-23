8422c11afd4d71c43617f39d88eedac029aa55d4
sunilm33 <78884617+sunilm33@users.noreply.github.com>
2024-03-23 15:01:17 -0700

    Able to make a video with audio

--- backend/generate_video_2.py
+++ backend/generate_video_2.py
@@ -1,11 +1,19 @@
 import moviepy.editor as mpe
+import os
+import random
 nextInt = 0
-# Get video/audio
-v_clip = mpe.VideoFileClip('../template_videos/minecraft_parkour.mp4')
-v_clip = v_clip.subclip(0, 5) 
+# Replace the next string with the output string from get audio
+# Get audio
 audio_background = mpe.AudioFileClip('../template_videos/reddit_audio.mp3')
-audio_background = audio_background.subclip(0, 5) 
-# clip together
-final_audio = mpe.CompositeAudioClip([v_clip.audio, audio_background])
-v_clip.audio = final_audio
-v_clip.write_videofile(f"../output_video/newVideo{nextInt}.mp4")
+audio_background = audio_background.subclip(0,180)
+audio_duration = audio_background.duration
+# Subclip a random part of it 
+# Get video
+v_clip = mpe.VideoFileClip('../template_videos/minecraft_parkour.mp4')
+video_start = random.randint(0, ((int)(v_clip.duration - audio_duration)) + 1)
+video_end = video_start + audio_duration + 1
+v_clip = v_clip.subclip(video_start, video_end)
+v_clip = v_clip.set_audio(audio_background)
+# Create a new video with the name
+videos_created = len(os.listdir('../output_video/'))
+v_clip.write_videofile(f"../output_video/newVideo{videos_created}.mp4")
